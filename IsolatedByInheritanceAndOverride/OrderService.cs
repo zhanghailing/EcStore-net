0a9ee17213afee4ef3216ecfc34c709889fd9258
hatelove <jay.hatelove@gmail.com>
2018-01-26 21:39:23 +0800

    extract and override practices

--- IsolatedByInheritanceAndOverride/OrderService.cs
+++ IsolatedByInheritanceAndOverride/OrderService.cs
@@ -86,8 +86,7 @@ namespace IsolatedByInheritanceAndOverride
         {
             // directly depend on some web service
             //var client = new HttpClient();
-            //var response = client.PostAsync<Order>("http://api.joey.io/Order", order, new JsonMediaTypeFormatter()).Result;
-
+            //var response = client.PostAsync<Order>("http://api.joey.io/Order", order, new JsonMediaTypeFormatter()).Result; 
             //response.EnsureSuccessStatusCode();
             throw new NotImplementedException();
         }
