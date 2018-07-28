486bfb05189cb5bbcecfd809d430771dcd0754b2
hatelove <jay.hatelove@gmail.com>
2018-07-28 16:49:18 +0800

    extract and oerride for dependency injection

--- IsolatedByInheritanceAndOverride.Test/OrderServiceTest.cs
+++ IsolatedByInheritanceAndOverride.Test/OrderServiceTest.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Text;
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using NSubstitute;
 using NUnit.Framework;
-using TestContext = Microsoft.VisualStudio.TestTools.UnitTesting.TestContext;
+using System.Collections.Generic;
 
 namespace IsolatedByInheritanceAndOverride.Test
 {
@@ -29,19 +25,14 @@ namespace IsolatedByInheritanceAndOverride.Test
         ///</summary>
         public TestContext TestContext
         {
-            get
-            {
-                return testContextInstance;
-            }
-            set
-            {
-                testContextInstance = value;
-            }
+            get { return testContextInstance; }
+            set { testContextInstance = value; }
         }
 
         #region 其他測試屬性
+
         //
-        // 您可以使用下列其他屬性撰寫您的測試: 
+        // 您可以使用下列其他屬性撰寫您的測試:
         //
         // 執行該類別中第一項測試前，使用 ClassInitialize 執行程式碼
         // [ClassInitialize()]
@@ -51,7 +42,7 @@ namespace IsolatedByInheritanceAndOverride.Test
         // [ClassCleanup()]
         // public static void MyClassCleanup() { }
         //
-        // 在執行每一項測試之前，先使用 TestInitialize 執行程式碼 
+        // 在執行每一項測試之前，先使用 TestInitialize 執行程式碼
         // [TestInitialize()]
         // public void MyTestInitialize() { }
         //
@@ -59,15 +50,53 @@ namespace IsolatedByInheritanceAndOverride.Test
         // [TestCleanup()]
         // public void MyTestCleanup() { }
         //
-        #endregion
+
+        #endregion 其他測試屬性
 
         [Test]
         public void Test_SyncBookOrders_3_Orders_Only_2_book_order()
         {
-            // hard to isolate dependency to unit test
+            var target = new OrderServiceForTest();
+            target.SetOrders(new List<Order>
+            {
+                new Order {Type = "Book"},
+                new Order {Type = "CD"},
+                new Order {Type = "Book"},
+            });
 
-            //var target = new OrderService();
-            //target.SyncBookOrders();
+            var mockBookDao = Substitute.For<IBookDao>();
+            target.SetBookDao(mockBookDao);
+
+            target.SyncBookOrders();
+
+            mockBookDao.Received(2)
+                .Insert(Arg.Is<Order>(o => o.Type == "Book"));
+        }
+    }
+
+    public class OrderServiceForTest : OrderService
+    {
+        private List<Order> _orders;
+        private IBookDao _dao;
+
+        public void SetBookDao(IBookDao dao)
+        {
+            this._dao = dao;
+        }
+
+        protected override IBookDao GetBookDao()
+        {
+            return this._dao;
+        }
+
+        public void SetOrders(List<Order> orders)
+        {
+            this._orders = orders;
+        }
+
+        protected override List<Order> GetOrders()
+        {
+            return this._orders;
         }
     }
 }
--- IsolatedByInheritanceAndOverride/OrderService.cs
+++ IsolatedByInheritanceAndOverride/OrderService.cs
@@ -19,14 +19,19 @@ namespace IsolatedByInheritanceAndOverride
             // only get orders of book
             var ordersOfBook = orders.Where(x => x.Type == "Book");
 
-            var bookDao = new BookDao();
+            var bookDao = GetBookDao();
             foreach (var order in ordersOfBook)
             {
                 bookDao.Insert(order);
             }
         }
 
-        private List<Order> GetOrders()
+        protected virtual IBookDao GetBookDao()
+        {
+            return new BookDao();
+        }
+
+        protected virtual List<Order> GetOrders()
         {
             // parse csv file to get orders
             var result = new List<Order>();
@@ -80,7 +85,12 @@ namespace IsolatedByInheritanceAndOverride
         public string CustomerName { get; set; }
     }
 
-    public class BookDao
+    public interface IBookDao
+    {
+        void Insert(Order order);
+    }
+
+    public class BookDao : IBookDao
     {
         public void Insert(Order order)
         {
