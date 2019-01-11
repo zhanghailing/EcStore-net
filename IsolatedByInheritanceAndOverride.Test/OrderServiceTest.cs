f238372201771668a1711a49ef9fbddc0362bcdf
hatelove <jay.hatelove@gmail.com>
2019-01-11 17:38:58 +0800

    complete test

--- IsolatedByInheritanceAndOverride.Test/OrderServiceTest.cs
+++ IsolatedByInheritanceAndOverride.Test/OrderServiceTest.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using NSubstitute;
+using NUnit.Framework;
+using System.Collections.Generic;
 
 namespace IsolatedByInheritanceAndOverride.Test
 {
@@ -11,10 +13,57 @@ namespace IsolatedByInheritanceAndOverride.Test
         [Test]
         public void Test_SyncBookOrders_3_Orders_Only_2_book_order()
         {
-            // hard to isolate dependency to unit test
-
+            var orderService = new FakeOrderService();
+            orderService.SetOrders(new List<Order>
+            {
+                new Order()
+                {
+                    Type = "Book"
+                },
+                new Order()
+                {
+                    Type = "CD"
+                },
+                new Order()
+                {
+                    Type = "Book"
+                },
+            });
             //var target = new OrderService();
-            //target.SyncBookOrders();
+
+            var bookDao = Substitute.For<IBookDao>();
+            orderService.SetBookDao(bookDao);
+
+            orderService.SyncBookOrders();
+
+            bookDao.Received(2).Insert(Arg.Is<Order>(order => order.Type == "Book"));
+            bookDao.DidNotReceive().Insert(Arg.Is<Order>(order => order.Type == "CD"));
+        }
+    }
+
+    internal class FakeOrderService : OrderService
+    {
+        private List<Order> _orders;
+        private IBookDao _bookDao;
+
+        public void SetBookDao(IBookDao bookDao)
+        {
+            _bookDao = bookDao;
+        }
+
+        protected override IBookDao GetBookDao()
+        {
+            return _bookDao;
+        }
+
+        public void SetOrders(List<Order> orders)
+        {
+            _orders = orders;
+        }
+
+        protected override List<Order> GetOrders()
+        {
+            return _orders;
         }
     }
 }
--- IsolatedByInheritanceAndOverride/OrderService.cs
+++ IsolatedByInheritanceAndOverride/OrderService.cs
@@ -19,14 +19,19 @@ namespace IsolatedByInheritanceAndOverride
             // only get orders of book
             var ordersOfBook = orders.Where(x => x.Type == "Book");
 
-            var bookDao = new BookDao();
+            var bookDao = GetBookDao();
             foreach (var order in ordersOfBook)
             {
                 bookDao.Insert(order);
             }
         }
 
-        private List<Order> GetOrders()
+        protected virtual IBookDao GetBookDao()
+        {
+            return new BookDao();
+        }
+
+        protected virtual List<Order> GetOrders()
         {
             // parse csv file to get orders
             var result = new List<Order>();
@@ -80,9 +85,14 @@ namespace IsolatedByInheritanceAndOverride
         public string CustomerName { get; set; }
     }
 
-    public class BookDao
+    public interface IBookDao
     {
-        internal void Insert(Order order)
+        void Insert(Order order);
+    }
+
+    public class BookDao : IBookDao
+    {
+        public void Insert(Order order)
         {
             // directly depend on some web service
             //var client = new HttpClient();
